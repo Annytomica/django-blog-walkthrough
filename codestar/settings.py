28177f8075d5b279bba71131c023d0ef07bc1a49
Anny Devoy <annydevoyalt@gmail.com>
2024-09-05 11:26:42 +0000

    Add gunicorn package for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tak7x7tez&s1#dk$&h-_r_&jjbvnjtup&yolj&*9w%0e@hlsuu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-annytomica-djangoblogwa-kwmvdp9ari5.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-annytomica-djangoblogwa-kwmvdp9ari5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
